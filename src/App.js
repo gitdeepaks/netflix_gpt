526c3d6f2b001ad5328c1b4825940a9f2ecb5f8b
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-25 11:58:30 +0530

    initialize the project 🚀

--- src/App.js
+++ src/App.js
@@ -1,21 +1,9 @@
-import logo from "./logo.svg";
 import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Wrold</p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1 className="text-3xl font-bold underline">Lets Build Netflix</h1>
     </div>
   );
 }
