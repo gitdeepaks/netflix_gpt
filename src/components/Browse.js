0bce02e5c7d9a240e239c4c1ce0c35e90bc5b0df
gitdeepaks <deepaksankhyan92@outlook.com>
2023-10-29 12:07:38 +0530

    added browse componenets

--- src/components/Browse.js
+++ src/components/Browse.js
@@ -1,12 +1,24 @@
-import React, { useEffect } from "react";
 import Header from "./Header";
 import useNowPlayingMovies from "../hooks/useNowPlayingMovies";
+import MainContainer from "./MainContainer";
+import SecondaryContainer from "./SecondaryContainer";
 
 const Browse = () => {
   useNowPlayingMovies();
   return (
     <div>
       <Header />
+      <MainContainer />
+      <SecondaryContainer />
+      {/* 
+      MainConytaier
+        - VideoBaground
+        - VideoTitle
+       SecondaryContainer
+        - MovieList * n
+        - cards * n
+      
+      */}
     </div>
   );
 };
