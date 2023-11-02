baaf02506d0d72d2d38d81ec85ea7962bbaa666a
gitdeepaks <deepaksankhyan92@outlook.com>
2023-11-02 14:13:27 +0530

    added navigation on  Logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -13,6 +13,11 @@ const Header = () => {
   const navigate = useNavigate();
   const user = useSelector((store) => store.user);
   const showGptSearch = useSelector((store) => store.gpt.showGptSearch);
+
+  const handlenavigateToHome = () => {
+    navigate("/browse");
+  };
+
   const handleSignOut = () => {
     signOut(auth)
       .then(() => {})
@@ -54,8 +59,13 @@ const Header = () => {
   };
 
   return (
-    <div className="absolute flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
-      <img className="w-40 h-14" src={LOGO} alt="Netflix Logo" />
+    <div className="absolute cursor-pointer flex justify-between w-screen px-8 py-2 bg-gradient-to-b from-black z-10">
+      <img
+        className="w-40 h-14"
+        src={LOGO}
+        onClick={handlenavigateToHome}
+        alt="Netflix Logo"
+      />
       {user && (
         <div className="flex p-2">
           {showGptSearch && (
